fcfa8b2160d43eb207f7296ac1aa29d8ce0ca91a
timothee_weber <timothee.weber@gmail.com>
2019-12-27 15:15:44 +0100

    create member lib file

--- lib/membre.lib.php
+++ lib/membre.lib.php
@@ -3,7 +3,19 @@
 
 function getMembreById($id){
     $conn = getConnection();
-    $sql = 'SELECT * FROM membre WHERE id='.$id;
-    $result = $conn->query($sql);
-    return $result->fetch_all(MYSQLI_ASSOC);
+    $sql = 'SELECT * FROM membre WHERE id_membre='.$id;
+    if($result = $conn->query($sql)){
+        return $result->fetch_assoc();
+    }else{
+        return "";
+    }
+}
+function getAllMembre(){
+    $conn = getConnection();
+    $sql = 'SELECT * FROM membre';
+    if($result = $conn->query($sql)){
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }else{
+        return "";
+    }
 }
